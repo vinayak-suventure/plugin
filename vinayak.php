210b97191623a854c424771b902a205d77b3abfd
vinayak-suventure <127106750+vinayak-suventure@users.noreply.github.com>
2023-03-06 17:35:51 +0530

    Create vinayak.php

---
+++ vinayak.php
@@ -0,0 +1,8 @@
+<? php
+/*
+* Plugin Name: vinayak
+* Plugin URI: https://thewebdecor.in
+* Author: Vinayak
+* Author URI: https://thewebdecor.in
+* Description:Simple Post Like & Dislike System
+*/
